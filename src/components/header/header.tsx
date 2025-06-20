1b58e5d138da450e5a46d96cab1ce89d62cd84dc
Carvajal-Daniel <carvajal.dev@hotmail.com>
2025-06-20 10:25:47 -0300

    feat: update header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const HeaderPage = () => {
   return (
-    <nav className="fixed top-0  w-[2000px] mx-auto z-50 backdrop-blur-md bg-[#141416]/80 flex justify-between items-center py-5">
+    <nav className="fixed top-0 left-0 right-0 w-full max-w-[1900px] mx-auto z-50 backdrop-blur-md bg-[#141416]/80 flex justify-between items-center py-5 px-4">
       <div className="group">
         <Link className="flex gap-2 items-center" href="/">
           <div className="flex items-center h-8 md:w-[37px] md:h-[36px] w-8 gap-1 justify-center text-black bg-[#e3ecf3] rounded-full group-hover:bg-[#292929] group-hover:text-white transition-colors duration-300">
@@ -17,7 +17,7 @@ const HeaderPage = () => {
 
       <div>
         <Link href="#">
-          <button className=" cursor-pointer border px-3 py-1 md:py-2 md:px-4 hover:bg-[#f0f8ff] hover:text-black transition-colors duration-300 rounded">
+          <button className="cursor-pointer border px-3 py-1 md:py-2 md:px-4 hover:bg-[#f0f8ff] hover:text-black transition-colors duration-300 rounded">
             Contato
           </button>
         </Link>
